7229db9b179f1d251595178b5b29237f6239aa5f
abhishek-varma <66531249+abhishekvarma827@users.noreply.github.com>
2024-02-16 20:39:13 -0600

    Update NOTES IMP #1.sv

--- NOTES IMP #1.sv
+++ NOTES IMP #1.sv
@@ -1,76 +1,49 @@
+// WORKING WITH ID like DRV1, DRV2, ENV1. ENV2 (set_report_id_verbosity) 
+// Working with Individual Component like DRV | ENV (set_report_verbosity_level)
+// Working with entire Hierarchy like ENV has DRV & MON (set_report_verbosity_level_hier)
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-module tb;
-  int data = 5;
+class env extends uvm_env;
+  `uvm_component_utils(env)
   
-  initial 
-    begin
-    //`uvm_info("[TB_TOP]", "Hello"  ,UVM_NONE );
-    `uvm_info("TB_TOP",$sformatf("Value of the data is %0d",data),UVM_NONE); 
-      //If wanted to print the data along with the message use system function
+  function new(string name, uvm_component parent); 
+    super.new(name,parent);
+  endfunction
   
-    //To get the verbosity level as we have not extended any class we will be using             uvm_top to get the verbosity level (UVM_MEDIUM = 200)
-    $display("Default Verbosoity Level :%0d",uvm_top.get_report_verbosity_level);
-    
-      //As verbosity level is set as medium we will try to print with different                   levels and see
-      `uvm_info("TOP", " Verbosity is NONE ", UVM_NONE);
-      `uvm_info("TOP", " Verbosity is LOW ", UVM_LOW);
-      `uvm_info("TOP", " Verbosity is MEDIUM ", UVM_MEDIUM);
-      `uvm_info("TOP", " Verbosity is HIGH ", UVM_HIGH);
-      `uvm_info("TOP", " Verbosity is FULL ", UVM_FULL);
-      `uvm_info("TOP", " Verbosity is DEBUG ", UVM_DEBUG);
-      
-      #10
-      //Changing the verbosity level
-      uvm_top.set_report_verbosity_level(UVM_HIGH);
-      $display("Default verbosity level is :%0d",uvm_top.get_report_verbosity_level);
-      `uvm_info("TOP", " Verbosity is NONE ", UVM_NONE);
-      `uvm_info("TOP", " Verbosity is LOW ", UVM_LOW);
-      `uvm_info("TOP", " Verbosity is MEDIUM ", UVM_MEDIUM);
-      `uvm_info("TOP", " Verbosity is HIGH ", UVM_HIGH);
-      `uvm_info("TOP", " Verbosity is FULL ", UVM_FULL);
-      `uvm_info("TOP", " Verbosity is DEBUG ", UVM_DEBUG);
-      
-  end
-  
-endmodule
-
-
-/*
-
-//               WORKING WITH ID AND VERBOSITY
-
-`include "uvm_macros.svh"
-import uvm_pkg::*;
-
-
-  
-class driver extends uvm_driver;
-    `uvm_component_utils(driver)
-   
-    function new(string name, uvm_component parent);
-      super.new(name,parent);
-    endfunction
-  
-    task run();
-      `uvm_info("DRV1", "Executed the Driver 1 code",UVM_HIGH);
-      `uvm_info("DRV2","Executed the Driver 2 code",UVM_HIGH);
-    endtask
+  task run();
+    `uvm_info("ENV1","Executed ENV1 code",UVM_HIGH);
+    `uvm_info("ENV2","Executed ENV1 code",UVM_HIGH);
+  endtask
 endclass
+
+class driver extends uvm_driver;
+  `uvm_component_utils(driver)
   
+  function new(string name, uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  task run();
+    `uvm_info("DRV1","Executed DRV1 code",UVM_HIGH);
+    `uvm_info("DRV2","Executed DRV2 code",UVM_HIGH);
+  endtask
+endclass
+
 module tb;
-  driver drv;
+  env e;
+  driver d;
   
   initial begin
-    drv = new("DRV",null);
-    drv.set_report_id_verbosity("DRV2",UVM_HIGH);
-    drv.run();
+    e = new("ENV",null);
+    d = new("DRV",null);
+  //  d.set_report_id_verbosity("DRV1",UVM_HIGH);
+  //  e.set_report_id_verbosity("ENV1",UVM_FULL);
+    d.set_report_verbosity_level(UVM_HIGH);
+    e.run();
+    d.run();
+    
+//can set the entire UVM verbosity by writing the + UVM_VERBOSITY = UVM_HIGH (Run Options)
+    
   end
 endmodule
-
-
-
-
-
-*/
