e4239715a25769a203f2c2fe9678d502ea98fbb2
abhishek-varma <66531249+abhishekvarma827@users.noreply.github.com>
2024-02-16 20:39:44 -0600

    Update NOTES IMP #2.sv

--- NOTES IMP #2.sv
+++ NOTES IMP #2.sv
@@ -1,29 +1,80 @@
-//               WORKING WITH ID AND VERBOSITY
+// WORKING WITH ID like DRV1, DRV2, ENV1. ENV2 (set_report_id_verbosity) 
+// Working with Individual Component like DRV | ENV (set_report_verbosity_level)
 
+
+
+
+// Working with entire Hierarchy like ENV has DRV & MON (set_report_verbosity_level_hier)
 `include "uvm_macros.svh"
 import uvm_pkg::*;
-
-
-  
+ 
+//////////////////////////////////////////////////
 class driver extends uvm_driver;
-    `uvm_component_utils(driver)
-   
-    function new(string name, uvm_component parent);
-      super.new(name,parent);
-    endfunction
+  `uvm_component_utils(driver)
   
-    task run();
-      `uvm_info("DRV1", "Executed the Driver 1 code",UVM_HIGH);
-      `uvm_info("DRV2","Executed the Driver 2 code",UVM_HIGH);
-    endtask
+  function new(string path , uvm_component parent);
+    super.new(path, parent);
+  endfunction
+ 
+  
+  task run();
+    `uvm_info("DRV", "Executed Driver Code", UVM_HIGH);
+  endtask
+  
 endclass
+///////////////////////////////////////////////////
+ 
+class monitor extends uvm_monitor;
+  `uvm_component_utils(monitor)
   
-module tb;
+  function new(string path , uvm_component parent);
+    super.new(path, parent);
+  endfunction
+ 
+  
+  task run();
+    `uvm_info("MON", "Executed Monitor Code", UVM_HIGH);
+  endtask
+  
+endclass
+ 
+//////////////////////////////////////////////////
+ 
+class env extends uvm_env;
+  `uvm_component_utils(env)
+  
   driver drv;
+  monitor mon;
   
-  initial begin
-    drv = new("DRV",null);
-    drv.set_report_id_verbosity("DRV1",UVM_HIGH);
+  function new(string path , uvm_component parent);
+    super.new(path, parent);
+  endfunction
+  
+  
+  
+  task run();
+    drv = new("DRV", this);
+    mon = new("MON", this);
     drv.run();
+    mon.run();
+  endtask
+  
+endclass
+////////////////////
+ 
+ 
+module tb;
+  
+ 
+ env e;
+  
+  
+ initial begin
+   e  = new("ENV", null);
+   e.set_report_verbosity_level_hier(UVM_HIGH);
+   e.run(); 
   end
+  
+  
 endmodule
+ 
